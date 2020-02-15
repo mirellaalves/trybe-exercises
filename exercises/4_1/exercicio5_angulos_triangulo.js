b1dc865687e4051aa0f9e78039e2865d602616b1
mirellaalves <mirella.alves@gmail.com>
2020-02-15 19:47:48 -0300

    Exercicio 5 criado programa angulos do triangulo

---
+++ exercises/4_1/exercicio5_angulos_triangulo.js
@@ -0,0 +1,11 @@
+let a = 60
+let b = 90
+let c = 3
+
+if (a + b + c == 180) {
+    return true;
+  } 
+else {
+    alert("Para os ângulos serem de um triângulo válido, a soma dos três deve ser 180 graus.");
+    return false;
+  }
