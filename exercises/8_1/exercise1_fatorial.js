115d4a7b56a8a897cf08ccaeaa6c32937e5f8d25
mirellaalves <mirella.alves@gmail.com>
2020-03-19 11:52:58 -0300

    Exercicio 1 funcao normal.

--- exercises/8_1/exercise1_fatorial.js
+++ exercises/8_1/exercise1_fatorial.js
@@ -1,11 +1,13 @@
-let n = 5;
+const n = -1;
 let fatorial = 1
 
 if (n > 0) {
-    for (let i = n; i > 0; i--) {
+    for (let i = n; i > 0; i -= 1) {
         fatorial = fatorial * i;
     }
     console.log(fatorial);
-} else {
+} else if (n == 0) {
     console.log(1);
+} else {
+    return false;
 }
