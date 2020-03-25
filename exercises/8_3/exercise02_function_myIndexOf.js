bd843e8b509d2621c6eedde057580f3776b65855
mirellaalves <mirella.alves@gmail.com>
2020-03-25 16:56:11 -0300

    Revisao exercicio02 function myIndexOf.

--- exercises/8_3/exercise02_function_myIndexOf.js
+++ exercises/8_3/exercise02_function_myIndexOf.js
@@ -1,6 +1,6 @@
 /* A função myIndexOf(arr, item) recebe um array arr, um item e retorna o índice do item ou -1 caso o item não pertença ao array arr
-Teste se a chamada myIndexOf([1, 2, 3, 4], 3) retorna o valor esperado
-Teste se a chamada myIndexOf([1, 2, 3, 4], 5) retorna o valor esperado */
+1 Teste se a chamada myIndexOf([1, 2, 3, 4], 3) retorna o valor esperado
+2 Teste se a chamada myIndexOf([1, 2, 3, 4], 5) retorna o valor esperado */
 
 const assert = require('assert');
 
