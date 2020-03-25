c61bdedd01d4bcebe3d18f60461642869f26a387
mirellaalves <mirella.alves@gmail.com>
2020-03-25 16:50:20 -0300

    Finalizando exercicio01 function sum.

--- exercises/8_3/exercise01_function_sum.js
+++ exercises/8_3/exercise01_function_sum.js
@@ -1,8 +1,8 @@
 /* A função sum(a, b) retorna a soma do parâmetro a com o b
-Teste se o retorno de sum(4, 5) é 9
-Teste se o retorno de sum(0, 0) é 0
-Teste se a função sum lança um erro quando os parametros são 4 e "5"(string 5)
-Teste se a mensagem de erro é “parameters must be numbers” quando realizar a chamada sum(4, "5") */
+1 Teste se o retorno de sum(4, 5) é 9
+2 Teste se o retorno de sum(0, 0) é 0
+3 Teste se a função sum lança um erro quando os parametros são 4 e "5"(string 5)
+4 Teste se a mensagem de erro é “parameters must be numbers” quando realizar a chamada sum(4, "5") */
 
 const assert = require('assert');
 
@@ -14,9 +14,7 @@ function sum(a, b) {
   return a + b;
 }
 
-const test1 = sum(4, 5);
-assert.strictEqual(test1, 9, 'test1');
-const test2 = sum(0, 0);
-assert.strictEqual(test2, 0, 'test2');
-/* const test3 = sum(4, '5');
-aassert.ifError(test3, new Error('parameters must be numbers')); */
+assert.strictEqual(sum(4, 5), 9, 'test1');
+assert.strictEqual(sum(0, 0), 0, 'test2');
+assert.throws(() => { sum(4, '5'); }, /^Error: parameters must be numbers$/), 'test3';
+assert.throws(() => { sum(4, "5"); }, /^Error: parameters must be numbers$/, 'test4');
