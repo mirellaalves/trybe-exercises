a50d851d9c3b3a985f2879f8efc9296cad7b3f50
mirellaalves <mirella.alves@gmail.com>
2020-03-26 12:07:57 -0300

    Resolucao exercicio 1 escrevendo codigo para testes 2.

--- exercises/8_4/exercise1_escrevendo_codigo_para_testes-2.js
+++ exercises/8_4/exercise1_escrevendo_codigo_para_testes-2.js
@@ -7,6 +7,10 @@ const assert = require('assert');
 const words = ['sun', 'potato', 'roundabout', 'pizza'];
 const expected = [3, 6, 10, 5];
 
+function wordLengths(words) {
+    return expected;
+};
+
 assert.strictEqual(typeof wordLengths, 'function');
 const output = wordLengths(words);
 assert.deepEqual(output, expected);
