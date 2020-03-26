7399510747e8372319963573e8825226d1b2e14e
mirellaalves <mirella.alves@gmail.com>
2020-03-25 22:33:23 -0300

    Revisao exercicio07 variavel definida.

--- exercises/8_3/exercise07_variavel_definida.js
+++ exercises/8_3/exercise07_variavel_definida.js
@@ -3,16 +3,3 @@
 const assert = require('assert');
 
 const thereIs = '';
-
-function definedVariable(thereIs) {
-    if (thereIs !== undefined) {
-        return true;
-    }
-    return false;
-}
-
-assert.ok(definedVariable('', false, 'test1'));
-assert.ok(definedVariable('x', true, 'test2'));
-assert.ok(definedVariable(0, true, 'test3'));
-assert.ok(definedVariable(null, false, 'test4'));
-/*assert.ok(definedVariable(undefined, true, 'test5'));*/
