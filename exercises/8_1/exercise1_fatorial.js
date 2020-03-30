445b76ff3d1ef699e2b3dd6075381ee072da1175
mirellaalves <mirella.alves@gmail.com>
2020-03-30 11:53:33 -0300

    Revisao exercicio 01 e desenvolvimento exercicio 02.

--- exercises/8_1/exercise1_fatorial.js
+++ exercises/8_1/exercise1_fatorial.js
@@ -1,4 +1,8 @@
-const n = -1;
+/* 1 Crie uma função que receba um número e retorne seu fatorial.
+Na matemática, o fatorial de um número não negativo N, com a notação N!, é o produto de todos os inteiros menores ou iguais a N. Exemplo: 4! = 4 * 3 * 2 * 1 = 24.
+Bônus (opcional): tente fazer o mesmo exercício de forma recursiva . Spoiler: É possível resolver com uma linha. */
+
+const n = 4;
 let fatorial = 1
 
 if (n > 0) {
@@ -6,19 +10,20 @@ if (n > 0) {
         fatorial = fatorial * i;
     }
     console.log(fatorial);
-} else if (n == 0) {
+} else if (n === 0) {
     console.log(1);
 } else {
     return false;
 }
 
-/* em 1 linha:
-const fatorial = (n) => (n === 0) ? 1 : n * fatorial(n - 1);
+// em 1 linha:
+fatorial = (n) => (n === 0) ? 1 : n * fatorial(n - 1);
+console.log(n * fatorial(n - 1));
 
-É os mesmo que:
+// É os mesmo que:
 
 if (n === 0) {
     return 1
 } else {
-    return n * fatorial(n - 1)
-} */
+    return n * fatorial(n - 1);
+}
--- exercises/8_1/exercise2_maior_palavra.js
+++ exercises/8_1/exercise2_maior_palavra.js
@@ -1,9 +1,14 @@
-// Crie uma função que receba uma frase e retorne qual a maior palavra.
-// Exemplo:
-// longestWord("Antônio foi no banheiro e não sabemos o que aconteceu")
+/* Crie uma função que receba uma frase e retorne qual a maior palavra.
+
+Exemplo:
+      longestWord("Antônio foi no banheiro e não sabemos o que aconteceu") // retorna 'aconteceu' */
 
 
 let phrase = 'Antônio foi no banheiro e não sabemos o que aconteceu';
 let phraseArray = phrase.split(' ');
 let i;
 let words = phraseArray[0];
+
+for (i = 0; i < phraseArray.length; i += 1) {
+    
+}
