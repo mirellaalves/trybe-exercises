bf7cabc4f8dd3206bc0733fd5375023a346f62d2
mirellaalves <mirella.alves@gmail.com>
2020-03-30 20:02:17 -0300

    Resolucao exercicio 03 funcao personLikes.

--- exercises/9_3/exercise03_funcao_personLikes.js
+++ exercises/9_3/exercise03_funcao_personLikes.js
@@ -21,4 +21,4 @@ const gunnar = {
 const personLikes = ({name, age, likes}) => `${name} is ${age} years old and likes ${likes.join(", ")}.`
 assert.equal(personLikes(alex), "Alex is 26 years old and likes fly fishing.")
 assert.equal(personLikes(gunnar), "Gunnar is 30 years old and likes hiking, scuba diving, taking pictures.")
-    
+
