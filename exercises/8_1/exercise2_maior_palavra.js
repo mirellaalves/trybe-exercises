fa491af9a6637da4ca7a2a70abb24802174ca41b
mirellaalves <mirella.alves@gmail.com>
2020-04-04 15:17:51 -0300

    Revendo exercicio 2.

--- exercises/8_1/exercise2_maior_palavra.js
+++ exercises/8_1/exercise2_maior_palavra.js
@@ -1,14 +1,17 @@
 /* Crie uma função que receba uma frase e retorne qual a maior palavra.
 
 Exemplo:
-      longestWord("Antônio foi no banheiro e não sabemos o que aconteceu") // retorna 'aconteceu' */
+      longestWord("Antônio foi ao banheiro e não sabemos o que aconteceu") // retorna 'aconteceu' */
 
-
-let phrase = 'Antônio foi no banheiro e não sabemos o que aconteceu';
-let phraseArray = phrase.split(' ');
-let i;
-let words = phraseArray[0];
-
-for (i = 0; i < phraseArray.length; i += 1) {
-    
+const longestWord = (phrase) => {
+      phraseArr = phrase.split(' ');
+      i = 0;
+      for (word of phraseArr) {
+            if (word.length > i) {
+                  i = word.length;
+                  result = word;
+            }
+      }
+      return result
 }
+console.log(longestWord("Antônio foi ao banheiro e não sabemos o que aconteceu"));
