82c4c9123375e6c8175c79d60f5e7059a6ec0b7f
mirellaalves <mirella.alves@gmail.com>
2020-04-07 19:12:41 -0300

    Resolucao Exercicio 6 array com strings.

--- exercises/9_2/exercise6_array_com_strings.js
+++ exercises/9_2/exercise6_array_com_strings.js
@@ -1,5 +1,4 @@
 /* 6 Crie um array com strings no formato NOME_DO_LIVRO - GÊNERO_DO_LIVRO - NOME_DA_PESSOA_AUTORA
-
 Dica: Use a função map */
 
 const assert = require('assert');
@@ -78,6 +77,8 @@ const expected_result = [
 
 function formatedBookNames() {
   // escreva seu código aqui
+  return books.map(book => `${book.name} - ${book.genre} - ${book.author.name}`);
 }
+console.log(formatedBookNames());
 
 assert.deepEqual(formatedBookNames(), expected_result);
