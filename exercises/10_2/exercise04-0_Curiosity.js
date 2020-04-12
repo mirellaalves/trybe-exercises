982c896e47f11520bc15adfb269162267b6f449b
mirellaalves <mirella.alves@gmail.com>
2020-04-12 13:42:55 -0300

    Ajuste variavel exercicio 04-0 Curiosity e resoluao exercicio 04-1 Curiosity.

--- exercises/10_2/exercise04-0_Curiosity.js
+++ exercises/10_2/exercise04-0_Curiosity.js
@@ -9,8 +9,8 @@ const getMarsTemperature = () => {
 
 // crie a função sendMarsTemperature abaixo
 const sendMarsTemperature = () => {
-    const temperature = getMarsTemperature();
-    setTimeout(() => console.log(`Mars temperature is: ${temperature} degree Celsius`), messageDelay());
+    const currentTemperature = getMarsTemperature();
+    setTimeout(() => console.log(`Mars temperature is: ${currentTemperature} degree Celsius`), messageDelay());
 };
 
 sendMarsTemperature(); // imprime "Mars temperature is: 20 degree Celsius", por exemplo
