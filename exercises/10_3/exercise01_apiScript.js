20586cd3edde3950acaf13b00701128928845d3e
mirellaalves <mirella.alves@gmail.com>
2020-04-12 20:32:59 -0300

    Resolucao exercicio 01.

---
+++ exercises/10_3/exercise01_apiScript.js
@@ -0,0 +1,15 @@
+const API_URL = 'https://icanhazdadjoke.com/';
+
+const fetchJoke = () => {
+  // Adicionar lógica aqui!
+  const myObject = {
+    method: 'GET',
+    headers: { 'Accept': 'application/json' }
+  };
+  
+  fetch(API_URL, myObject)
+  .then(response => response.json())
+  .then(data => document.getElementById('jokeContainer').innerHTML = data.joke);
+};
+
+window.onload = () => fetchJoke();
