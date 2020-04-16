d18daaf0359b52301adfaa85ab1e5ce3786632ab
mirellaalves <mirella.alves@gmail.com>
2020-04-16 20:15:17 -0300

    Trying to solve exercise 10.

--- exercises/10_1/exercise10_compare_dois_objetos.test.js
+++ exercises/10_1/exercise10_compare_dois_objetos.test.js
@@ -1,5 +1,6 @@
 const obj = require('./exercise10_compare_dois_objetos');
 
 test('obj1 and ob2 are identicals', () => {
-  expect(obj1).toStrictEqual(obj2);
+  const obj1var = obj1;
+  expect(obj1var).toStrictEqual(obj2);
 });
