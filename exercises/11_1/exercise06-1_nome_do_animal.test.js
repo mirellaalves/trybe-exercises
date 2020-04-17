2095552e61158f3e1f934b60886fc9978f6cbfd2
mirellaalves <mirella.alves@gmail.com>
2020-04-17 12:59:19 -0300

    Solving exercise 06.1.

--- exercises/11_1/exercise06-1_nome_do_animal.test.js
+++ exercises/11_1/exercise06-1_nome_do_animal.test.js
@@ -1,3 +1,72 @@
 // 6 Nesse exercício, você irá criar funções parecidas com código abaixo - o mesmo que foi usado como exemplo sobre os testes de promise.
 
-const calcs = require('./exercise06-1_nome_do_animal')
+// const Animals = [
+//     { name: 'Dorminhoco', age: 1, type: 'Dog' },
+//     { name: 'Soneca', age: 2, type: 'Dog' },
+//     { name: 'Preguiça', age: 5, type: 'Cat' },
+//   ];
+  
+// const findAnimalsByType = (type) => (
+// new Promise((resolve, reject) => {
+//     setTimeout(() => {
+//     const arrayAnimals = Animals.filter((animal) => animal.type === type);
+//     if (arrayAnimals.length !== 0) {
+//         return resolve(arrayAnimals);
+//     }
+
+//     return reject({ error: 'Não possui esse tipo de animal.' });
+//     }, 100);
+// })
+// );
+  
+// const getListAnimals = (type) => (
+//     findAnimalsByType(type).then(list => list)
+// );
+
+// 6.1 Adicione uma funcionalidade para buscar pelo nome do animal - crie uma função que deverá passar no teste abaixo.
+// Dica, use o código usado como exemplo para criar uma nova função, analise os testes antes de iniciar.
+
+const Animals = [
+    { name: 'Dorminhoco', age: 1, type: 'Dog' },
+    { name: 'Soneca', age: 2, type: 'Dog' },
+    { name: 'Preguiça', age: 5, type: 'Cat' },
+];
+
+const findAnimalByName = (name) => (
+    // Adicione o código aqui.
+new Promise((resolve, reject) => {
+    setTimeout(() => {
+    const arrayAnimals = Animals.filter((animal) => animal.name === name);
+    if (arrayAnimals.length !== 0) {
+        return resolve(...arrayAnimals);
+    }
+    return reject('Nenhum animal com esse nome!');
+    }, 100);
+})
+)
+
+const getAnimal = (name) => {
+    // Adicione o código aqui.
+    return findAnimalByName(name).then(list => list)
+}
+// ---------------------
+
+describe('Testando promise - findAnimalByName', () => {
+    describe('Quando existe o animal com o nome procurado', () => {
+    test('Retorne o objeto do animal', () => {
+        expect.assertions(1);
+        return getAnimal('Dorminhoco').then(animal => {
+        expect(animal).toEqual({ name: 'Dorminhoco', age: 1, type: 'Dog' });
+        });
+    });
+    });
+
+    describe('Quando não existe o animal com o nome procurado', () => {
+    test('Retorna um erro', () => {
+        expect.assertions(1);
+        return getAnimal('Bob').catch(error =>
+        expect(error).toEqual('Nenhum animal com esse nome!')
+        );
+    });
+    });
+});
