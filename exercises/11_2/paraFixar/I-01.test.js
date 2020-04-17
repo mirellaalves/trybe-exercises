6dee655c73f4e3c5ad2c327b878ef84e97909173
mirellaalves <mirella.alves@gmail.com>
2020-04-17 18:34:34 -0300

    Solving exercise 01 Para Fixar I.

--- exercises/11_2/paraFixar/I-01.test.js
+++ exercises/11_2/paraFixar/I-01.test.js
@@ -1,5 +1,6 @@
 // 1 Baseando-se nos códigos acima, defina uma função que retorna um número aleatório entre 1 e 100. A seguir, defina uma outra função que recebe um parâmetro e checa se o número aleatório é divisível pelo número do parâmetro.
 // Feito isso, escreva um teste que verifica que a função randomNumber() é chamada quando invocamos a isDivisible().
+// 2 Teste que, quando a randomNumber() retorna um número par e isDivisible() recebe um dois, o retorno é true.
 
 function randomNumber() {
   Math.floor(Math.random() * 100) + 1;
@@ -10,8 +11,13 @@ function isDivisible(number) {
 }
 
 test('verifica que a função randomNumber() é chamada quando invocamos a isDivisible()', () => {
-  // testando se a função foi chamada
   randomNumber = jest.fn();
   isDivisible();
   expect(randomNumber).toHaveBeenCalled();
+});
+
+test('quando a randomNumber() retorna um número par e isDivisible() recebe um dois, o retorno é true', () => {
+  randomNumber = jest.fn();
+  isDivisible(2);
+  expect(randomNumber).toBeTruthy();
 });
