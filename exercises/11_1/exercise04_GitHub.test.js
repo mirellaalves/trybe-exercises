818ba6bff0bc24d237605200b8660e4263196fe9
mirellaalves <mirella.alves@gmail.com>
2020-04-17 11:05:13 -0300

    Solving exercise 04.

--- exercises/11_1/exercise04_GitHub.test.js
+++ exercises/11_1/exercise04_GitHub.test.js
@@ -1,3 +1,18 @@
-// 4 O código abaixo busca no GitHub de um usuário, de acordo com a URL, seus repositórios e retorna uma lista como resultado. Dada a URL 'https://api.github.com/users/tryber/repos', faça um teste que verifique que os repositórios 'sd-01-week4-5-project-todo-list' e 'sd-01-week4-5-project-meme-generator' se encontram nessa lista.
+// 4 O código abaixo busca no GitHub de um usuário, de acordo com a URL, seus repositórios e retorna uma lista como resultado. Dada a URL 'https://api.github.com/users/tryber/repos', faça um teste que verifique que os repositórios 'sd-01-block22-project-queries-unite' e 'sd-01-week19-recipes-app-2' se encontram nessa lista.
+// OBS.: repos do exercício alterados, pois não existem no api indicado.
 
-const calcs = require('./exercise04_GitHub')
+const fetch = require('node-fetch');
+
+const getRepos = (url) => {
+  return fetch(url)
+    .then(response => response.json())
+    .then((data) => {
+      return data.map((repo) => repo.name)
+    });
+}
+
+test('Repos are on the list', async () => {
+  const repos = await getRepos('https://api.github.com/users/tryber/repos');
+  expect(repos).toContain('sd-01-block22-project-queries-unite');
+  expect(repos).toContain('sd-01-week19-recipes-app-2');
+});
