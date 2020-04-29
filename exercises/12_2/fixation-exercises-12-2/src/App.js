7d17db6c07b700cc0700c9403679df3aff34fd42
mirellaalves <mirella.alves@gmail.com>
2020-04-28 21:30:10 -0300

    Solving fixation axercises a and b.

---
+++ exercises/12_2/fixation-exercises-12-2/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Image from './Image'
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <Image
+      source="https://cdn.pixabay.com/photo/2017/02/20/18/03/cat-2083492_1280.jpg"
+      alternativeText="Cute cat staring"
+      />
+    </div>
+  );
+}
+
+export default App;
