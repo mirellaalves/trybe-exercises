76c8ccea75717f7c4c35dc21e203b317b0d7b7a1
mirellaalves <mirella.alves@gmail.com>
2020-08-04 13:50:30 -0300

    ex13

--- exercises/24_1/ex13.js
+++ exercises/24_1/ex13.js
@@ -1,1 +1,5 @@
 // Exercício 13: Para os filmes Batman ou Home Alone, atribua a imdbRating o valor 17 caso o valor de imdbRating seja menor que 17.
+db.movies.updateMany(
+  { $or: [{title: "Batman"}, {title: "Home Alone"}] },
+  {$max: {imdbRating: 17}}
+);
