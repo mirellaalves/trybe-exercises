0f1d6d8e167eeefa8d698dab50cda49af626980e
mirellaalves <mirella.alves@gmail.com>
2020-08-10 16:44:10 -0300

    ex1 to ex4

--- exercises/25_1/ex2.js
+++ exercises/25_1/ex2.js
@@ -1,2 +1,10 @@
 // Exercício 2: Utilizando o estágio $match, escreva uma agregação para retornar somente os clientes do sexo "FEMININO" e com data de nascimento entre os anos de 1995 e 2005.
-
+db.clientes.aggregate([
+  { $match: {
+    sexo: "FEMININO",
+    dataNascimento: {
+      $gte: ISODate("1995-01-01T00:00:00Z"),
+      $lte: ISODate("2005-12-31T23:59:59Z")
+    }
+  } }
+]);
--- exercises/25_1/ex3.js
+++ exercises/25_1/ex3.js
@@ -1,2 +1,11 @@
 // Exercício 3: Utilizando o estágio $match, escreva uma agregação para retornar somente os clientes do sexo "FEMININO" e com data de nascimento entre os anos de 1995 e 2005, limitando a quantidade de documentos retornados em 5.
-
+db.clientes.aggregate([
+  { $match: {
+    sexo: "FEMININO",
+    dataNascimento: {
+      $gte: ISODate("1995-01-01T00:00:00Z"),
+      $lte: ISODate("2005-12-31T23:59:59Z")
+    }
+  } },
+  { $limit : 5 }
+]);
