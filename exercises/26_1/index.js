59c9e1e6dd1847afa4293a28b043aca833b7cfea
mirellaalves <mirella.alves@gmail.com>
2020-08-18 17:10:34 -0300

    ex1

--- exercises/26_1/index.js
+++ exercises/26_1/index.js
@@ -1,1 +1,10 @@
 const readline = require('readline-sync');
+
+// IMC = peso / altura * altura
+
+function executaCalculo () {
+  const peso = readline.questionFloat("Qual o seu peso em kg? (ex.: 82.300) ").toFixed(3);
+  const altura = readline.questionFloat("Qual a sua altura em m? (ex.: 1.75) ").toFixed(2);
+}
+
+executaCalculo();
