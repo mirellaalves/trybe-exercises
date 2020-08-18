489daf5f707cdc8acc265789a310bf9a08088e36
mirellaalves <mirella.alves@gmail.com>
2020-08-18 18:12:01 -0300

    ex2 improves

--- exercises/26_1/index.js
+++ exercises/26_1/index.js
@@ -1,12 +1,9 @@
 const readline = require('readline-sync');
 
-// IMC = peso / (altura * altura)
-
 function executaCalculo () {
   const peso = readline.questionFloat("Qual o seu peso em kg? (ex.: 82.300) = ").toFixed(3);
   const altura = readline.questionFloat("Qual a sua altura em m? (ex.: 1.75) = ").toFixed(2);
   const IMC = (peso / (altura * altura)).toFixed(2);
-
   console.log("Seu IMC é %s.", IMC);
 
   switch (true) {
@@ -28,7 +25,11 @@ function executaCalculo () {
     default:
       console.log('Você está com obesidade grau III ou IV.');
       break;
-  }
+  };
+
+  const pesoIdealMin = (18.5 * altura * altura).toFixed(1);
+  const pesoIdealMax = (24.99999 * altura * altura).toFixed(1);
+  console.log("Seu peso ideal é entre %skg e %skg.", pesoIdealMin, pesoIdealMax);
 };
 
 executaCalculo();
