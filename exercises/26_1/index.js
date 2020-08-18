b1549970a25356cfd5cf03065b4dbb87235dd740
mirellaalves <mirella.alves@gmail.com>
2020-08-18 17:37:49 -0300

    ex2

--- exercises/26_1/index.js
+++ exercises/26_1/index.js
@@ -1,10 +1,34 @@
 const readline = require('readline-sync');
 
-// IMC = peso / altura * altura
+// IMC = peso / (altura * altura)
 
 function executaCalculo () {
-  const peso = readline.questionFloat("Qual o seu peso em kg? (ex.: 82.300) ").toFixed(3);
-  const altura = readline.questionFloat("Qual a sua altura em m? (ex.: 1.75) ").toFixed(2);
-}
+  const peso = readline.questionFloat("Qual o seu peso em kg? (ex.: 82.300) = ").toFixed(3);
+  const altura = readline.questionFloat("Qual a sua altura em m? (ex.: 1.75) = ").toFixed(2);
+  const IMC = (peso / (altura * altura)).toFixed(2);
+
+  console.log("Seu IMC é %s.", IMC);
+
+  switch (true) {
+    case (IMC < 18.5):
+      console.log('Você está abaixo do peso (magreza).');
+      break;
+    case (IMC >= 18.5 && IMC < 25):
+      console.log('Você está com o peso normal.');
+      break;
+    case (IMC >= 25 && IMC < 30):
+      console.log('Você está acima do peso (sobrepeso).');
+      break;
+    case (IMC >= 30 && IMC < 35):
+      console.log('Você está com obesidade grau I.');
+      break;
+    case (IMC >= 35 && IMC < 40):
+      console.log('Você está com obesidade grau II.');
+      break;
+    default:
+      console.log('Você está com obesidade grau III ou IV.');
+      break;
+  }
+};
 
 executaCalculo();
