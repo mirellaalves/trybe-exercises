e04aca515ce5829e4275f9403bb08c4c80776cc5
mirellaalves <mirella.alves@gmail.com>
2020-09-01 17:26:46 -0300

    join bonus to simpsons app and try to execute

--- exercises/27_1/index.js
+++ exercises/27_1/index.js
@@ -9,6 +9,7 @@
 const fs = require('fs');
 const express = require('express');
 const bodyParser = require('body-parser');
+const crypto = require('crypto');
 
 const app = express();
 
@@ -17,15 +18,6 @@ app.use(bodyParser.urlencoded({extended: true}));
 
 // 7 Crie um endpoint do tipo GET na rota /simpsons que deve retornar a lista completa de personagens.
 const simpsons = 'simpsons.json';
-
-// fs.readFile(simpsons, 'utf8', (err, data) => {
-//   if (err) throw err;
-//   file = JSON.parse(data);
-// });
-
-// app.get('/simpsons', (_req, res) => {
-//   res.status(200).json(file);
-// });
 
 app.get('/simpsons', (_req, res) => {
   fs.readFile(simpsons, 'utf-8', (err, data) => {
@@ -48,19 +40,6 @@ app.get('/simpsons/:id', (req, res) => {
 // O corpo da requisição deve receber os campos id e name;
 // Bônus: A operação só deve ser realizada caso o campo id seja único. Caso contrário, deve ser retornado status 400.
 
-// app.post('/simpsons', (req, res) => {
-//   const id = req.body.id;
-//   const name = req.body.name;
-//   newData = [...file, { id, name }];
-//   if (!id) {
-//     res.status(400).json({ message: 'Bad Request' });
-//   } else {
-//     res.status(200).json(fs.writeFile(simpsons, JSON.stringify(newData), (err) => {
-//       if (err) throw err;
-//     }));
-//   }
-// });
-
 app.post('/simpsons', (req, res) => {
   const id = req.body.id;
   const name = req.body.name;
@@ -74,11 +53,54 @@ app.post('/simpsons', (req, res) => {
   });
 });
 
+function generateToken () {
+  return crypto.randomBytes(8).toString('hex');
+}
+
+const authMiddleware = (req, res, next) => {
+  const {token} = req.headers;
+  if (token && token.length === 16) return next();
+  return res.status(401).json({ message: 'Token inválido!' });
+};
+
+app.use('/simpsons', authMiddleware);
+
+app.post('/signup', (req, res) => {
+  const email = req.body.email;
+  const password = req.body.password;
+  const firstName = req.body.firstName;
+  const phone = req.body.phone;
+  if (email !== '' && password !== '' && firstName !== '' && phone !== '') {
+    res.status(200).json({ token: {generateToken} });
+  }
+});
+
 app.use((err, _req, res, _next) => {
   console.error(err);
   res.status(500).json({ error: err.message });
 });
 
+app.all('*', (_req, res) => {
+  res.status(404).json({ message: 'Not Found' });
+});
+
 app.listen(3000, () => {
   console.log('Ouvindo a porta 3000!')
 });
+
+// Bônus
+// 1 Caso algum endpoint que não consta nessa lista receber uma requisição, a resposta deve voltar com status 404 Not Found, sem corpo.
+
+// 2 Todos os endpoints devem receber um token de autenticação. Esse token deve vir no seguinte modelo: authorization: {token-aqui}. Nesse momento, como você não está salvando os tokens gerados no cadastro em nenhum lugar, só é preciso verificar que as requisições recebem um token no header que possua 16 caracteres.
+// Caso o token seja inválido ou inexistente, a resposta deve voltar com status 401 Unauthorized e { message: 'Token inválido!' } no corpo.
+
+// 3 O caminho do endpoint de autenticação deve ser /signup.
+// As requisições para esse endpoint deverão utilizar o método POST e devem receber, no corpo da requisição, os campos email, password, firstName e phone.
+// Caso todos os requisitos acima sejam atendidos, um token aleatório composto por letras e números, com 16 caracteres, deve ser retornado. Para gerar o token você pode utilizar a função randomBytes, do módulo crypto do node, dessa forma:
+// generate-token.js
+// const crypto = require('crypto');
+// function generateToken () {
+//   return crypto.randomBytes(8).toString('hex');
+// }
+// module.exports = generateToken;
+// A resposta da requisição deve ser um objeto com o formato { token: <novo-token-gerado> }.
