abf29867392a3e8c17293b64a1f1639c460ca2a8
mirellaalves <mirella.alves@gmail.com>
2020-09-07 17:32:28 -0300

    finish mvc example about authors

--- exercises/27_2/mvc_example/index.js
+++ exercises/27_2/mvc_example/index.js
@@ -1,5 +1,32 @@
-const connection = require('./models/connection');
+// const connection = require('./models/connection');
 
-connection().then((session) => {
-  console.log('Conectado ao MySQL!');
+// connection().then((session) => {
+//   console.log('Conectado ao MySQL!');
+// });   // SUBSTITUÍDO PELO CONTEÚDO ABAIXO
+
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const authorController = require('./controllers/authorController');
+
+const app = express();
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.set('view engine', 'ejs');
+
+app.set('views', './views');
+
+app.get('/authors', authorController.listAuthors);
+
+app.get('/authors/new', authorController.newAuthor);
+
+app.get('/authors/:id', authorController.showAuthor);
+
+app.post('/authors', authorController.createAuthor);
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Ouvindo a porta ${PORT}`);
 });
--- exercises/27_2/mvc_example/models/Author.js
+++ exercises/27_2/mvc_example/models/Author.js
@@ -39,6 +39,53 @@ const getAll = async () =>
           })
         )
     );
+
+/**
+ * Busca um autor específico, a partir do seu ID
+ * @param {String} id ID do autor a ser recuperado
+ */
+const findById = async (id) => {
+  const authorData = await connection()
+    .then((db) =>
+      db
+        .getTable('authors')
+        .select(['first_name', 'middle_name', 'last_name'])
+        .where('id = :id')
+        .bind('id', id)
+        .execute()
+    )
+    .then((results) => results.fetchAll())
+    .then((authors) => authors[0]);
+
+  if (!authorData) return null;
+
+  const [firstName, middleName, lastName] = authorData;
+
+  return getNewAuthor({ id, firstName, middleName, lastName });
+};
+
+// Retorna um boolean indicando se os dados são válidos, checando se firstName e lastName são strings não vazias, e se middleName, caso seja informado, é uma string.
+const isValid = (firstName, middleName, lastName) => {
+  if (!firstName || typeof firstName !== 'string') return false;
+  if (!lastName || typeof lastName !== 'string') return false;
+  if (middleName && typeof middleName !== 'string') return false;
+
+  return true;
+};
+
+// Salva um autor no banco, utilizando os métodos insert e values. insert, assim como select, recebe um array de strings com os nomes das colunas, enquanto values recebe os valores a serem inseridos.
+const create = async (firstName, middleName, lastName) =>
+  connection().then((db) =>
+    db
+      .getTable('authors')
+      .insert(['first_name', 'middle_name', 'last_name'])
+      .values(firstName, middleName, lastName)
+      .execute()
+  );
+
 module.exports = {
   getAll,
+  findById,
+  isValid,
+  create,
 };
